a3d948195f4b6cd84c523c11c6368b00e251c316
andres castillo <andcastillo@gmail.com>
2019-09-20 20:41:53 -0500

    add correction factor 2

--- src/compareSpinus.js
+++ src/compareSpinus.js
@@ -14,7 +14,7 @@ let folder = '/Users/acastillo//Documents/dataNMR/spinus/'
 let data = fs.readdirSync(folder).filter(file => file.indexOf('.mol') >= 0);
 
 let nMols = data.length;
-let nSamples = 200;
+let nSamples = 400;
 let p = new Predictor({ db });
 
 // let stats = { min: Number.MAX_VALUE, max: Number.MIN_VALUE, sum: 0, count: 0 };
